ed047e98e6997f87b818d30ba4f919a57e441c57
NenadM1989 <105055911+NenadM1989@users.noreply.github.com>
2022-12-19 10:16:24 +0100

    modals

--- flowrspot/src/pages/Profile/Profile.js
+++ flowrspot/src/pages/Profile/Profile.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./Profile.module.css";
+import image from "../../images/profile-picture.png";
 
 const Profile = () => {
-  return <div className={styles.profile}></div>;
+  return (
+    <div className={styles.profile}>
+      <div className={styles.close}>X</div>
+    </div>
+  );
 };
 
 export default Profile;
