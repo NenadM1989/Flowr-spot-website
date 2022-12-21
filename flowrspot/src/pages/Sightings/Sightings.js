718e33ed4139f6c880f2e38e19caea5e777033c7
NenadM1989 <105055911+NenadM1989@users.noreply.github.com>
2022-12-21 15:44:06 +0100

    new sightings list

--- flowrspot/src/pages/Sightings/Sightings.js
+++ flowrspot/src/pages/Sightings/Sightings.js
@@ -1,30 +1,20 @@
 import React from "react";
 import styles from "../Sightings/Sightings.module.css";
 import AddNewSightings from "../../components/Buttons/AddNewSightings";
+import image from "../../images/hero-sightings.png";
 
 const Sightings = () => {
   return (
     <div className={styles["sightings - container"]}>
-      <div className={styles["sightings-top"]}>
-        <div className={styles["sightings-content"]}>
-          <div className={styles["sightings-title"]}>
-            <p className={`${styles.paragraph} ${styles["first-paragraph"]}`}>
-              Sighting List
-            </p>
-          </div>
-          <div className={styles["button-add"]}>
-            <AddNewSightings />
-          </div>
+      <img src={image} alt="" className={styles.image} />
+      <div className={styles["sighting-list"]}>
+        <div className={styles["sightings-text"]}>
+          <p className={styles.p1}>Sighting list</p>
+          <p className={styles.p2}>Explore between more than 8.427 sightings</p>
         </div>
-        <div className={styles["sightings-explore"]}>
-          <p className={`${styles.paragraph} ${styles["second-paragraph"]}`}>
-            Explore between 8,427 sightings
-          </p>
+        <div className={styles["sightings-button"]}>
+          <AddNewSightings />
         </div>
-      </div>
-      <div></div>
-      <div className={styles["sighting-button"]}>
-        <AddNewSightings />
       </div>
     </div>
   );
