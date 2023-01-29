1c6fb13851b1cae02979c8b8314c7e2d6eca0290
NenadM1989 <105055911+NenadM1989@users.noreply.github.com>
2023-01-29 23:05:32 +0100

    localstorage

--- flowrspot/src/pages/CreateAnAccount/CreateAnAccount.js
+++ flowrspot/src/pages/CreateAnAccount/CreateAnAccount.js
@@ -1,33 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CreateAnAccount.module.css";
 
 const CreateAnAccount = ({ func }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+
+    setFirstName(""), setLastName(""), setEmail(""), setPassword("");
+  };
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>Create an Account</h1>
-      <div className={styles.names}>
-        <input placeholder="First Name" />
-        <input placeholder="Last Name" />
-      </div>
-      <div className={styles.column}>
-        <input
-          placeholder="Date of Birth"
-          type="date"
-          className={styles.date}
-        />
-        <input placeholder="Email Address" />
-        <input type="password" placeholder="Password" />
-        <button className={styles.button}>Create Account</button>
-      </div>
-      <div
-        onClick={() => {
-          func(false);
-        }}
-        className={styles.close}
-      >
-        I dont want to register
-      </div>
-    </div>
+    <>
+      <form className={styles.container} onSubmit={handleSubmit}>
+        <h1 className={styles.header}>Create an Account</h1>
+        <div className={styles.names}>
+          <input
+            placeholder="First Name"
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            placeholder="Last Name"
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className={styles.column}>
+          <input
+            placeholder="Email Address"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className={styles.button}>
+            Create Account
+          </button>
+        </div>
+        <div
+          onClick={() => {
+            func(false);
+          }}
+          className={styles.close}
+        >
+          I dont want to register
+        </div>
+      </form>
+    </>
   );
 };
 
--- flowrspot/src/pages/Login/Login.js
+++ flowrspot/src/pages/Login/Login.js
@@ -1,34 +1,55 @@
 import React, { useState } from "react";
 import styles from "./Login.module.css";
 
-const Login = ({ funct, funcLog }) => {
+const Login = ({ funct }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser.email === email && storedUser.password === password) {
+      console.log(email, password);
+    } else {
+      console.log("Error occurred!");
+    }
+  };
   return (
-    <div className={styles.login}>
-      <h1 className={styles.header}>Welcome Back</h1>
-      <div className={styles.data}>
-        <input placeholder="Email Adress" />
-        <input type="password" placeholder="Password" />
-      </div>
-      <div className={styles["login-button"]}>
-        <button
+    <>
+      <form className={styles.login} onSubmit={handleSubmit}>
+        <h1 className={styles.header}>Welcome Back</h1>
+        <div className={styles.data}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className={styles["login-button"]}>
+          <button
+            type="submit"
+            className={styles.button}
+            //onClick={() => {
+            //            }}
+          >
+            Login to your Account
+          </button>
+        </div>
+        <div
+          className={styles["close-login"]}
           onClick={() => {
-            funcLog(true);
             funct(false);
           }}
-          className={styles.button}
         >
-          Login to your Account
-        </button>
-      </div>
-      <div
-        onClick={() => {
-          funct(false);
-        }}
-        className={styles["close-login"]}
-      >
-        I dont want to Login
-      </div>
-    </div>
+          I dont want to Login
+        </div>
+      </form>
+    </>
   );
 };
 
